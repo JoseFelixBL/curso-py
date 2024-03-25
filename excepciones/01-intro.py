f051fe58ed21c9f47ee03161e47238db961ff243
José Bello <jfbello.work@gmail.com>
2024-03-25 18:49:26 +0100

    Completado capítulo de Excepciones

---
+++ excepciones/01-intro.py
@@ -0,0 +1,7 @@
+# print("")
+# lista = [1, 2]
+# lista[45]
+try:
+    n1 = int(input("Ingresa primer número: "))
+except:
+    print("Ocurrió un error :(")
---
+++ excepciones/02-tipos-de-excepciones.py
@@ -0,0 +1,10 @@
+try:
+    n1 = int(input("Ingresa primer número: "))
+    ntoheu
+# except Exception as e:
+#     print(type(e))
+#     print("Ocurrió un error :(")
+except ValueError as e:
+    print("Ingrese un valor que corresponda")
+except NameError as e:
+    print("Ocurrió un error")
