6960801da4d0b0d0fcb845a9ff4ce7370006e119
José Bello <jfbello.work@gmail.com>
2024-04-05 21:32:42 +0200

    DJANGO: proyecto, app-Productos, views y Models

---
+++ productly/productos/views.py
@@ -0,0 +1,11 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+# /productos
+
+
+def index(request):
+    return HttpResponse('Hola Mundo!')
